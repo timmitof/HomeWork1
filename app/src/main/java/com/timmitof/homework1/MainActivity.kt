5aae88625596a11b086cce810f05b95db5065730
timmitof <81027444+timmitof@users.noreply.github.com>
2021-11-05 10:23:37 +0600

    HomeWork 1

---
+++ app/src/main/java/com/timmitof/homework1/MainActivity.kt
@@ -0,0 +1,82 @@
+package com.timmitof.homework1
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var buttonSend: Button
+
+    lateinit var namef: EditText
+    lateinit var surnamef: EditText
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        namef = findViewById(R.id.nameIn)
+        surnamef = findViewById(R.id.surnameIn)
+        buttonSend = findViewById(R.id.btnSend)
+        buttonSend.setOnClickListener{
+            val name = namef.text.toString()
+            val surname = surnamef.text.toString()
+
+            if (name.isEmpty()){
+                namef.error = "Введите имя"
+                Toast.makeText(this, "Заполните поля!", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            else if (surname.isEmpty()){
+                surnamef.error = "Введите фамилию"
+                Toast.makeText(this, "Заполните поля!", Toast.LENGTH_LONG).show()
+                return@setOnClickListener
+            }
+
+            if (name != null && surname != null) {
+
+                val intent = Intent(this, SecondActivity::class.java)
+
+                intent.putExtra("NAME", name)
+                intent.putExtra("SURNAME", surname)
+
+                startActivity(intent)
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        Log.i("onStart", "Старт!")
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.i("onResume", "Воспроизведение!")
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Log.i("onPause", "Пауза!")
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("onStop", "Стоп!")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("onDestroy", "Уничтожение!")
+    }
+}
---
+++ app/src/main/java/com/timmitof/homework1/SecondActivity.kt
@@ -0,0 +1,28 @@
+package com.timmitof.homework1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
+
+class SecondActivity : AppCompatActivity() {
+    lateinit var textName: TextView
+    lateinit var backButton: ImageButton
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        textName = findViewById(R.id.tvName)
+        backButton = findViewById(R.id.backBtn)
+        val name = intent.getStringExtra("NAME")
+        val surname = intent.getStringExtra("SURNAME")
+
+        textName.text = name + " " + surname
+        backButton.setOnClickListener {
+            onBackPressed()
+        }
+
+    }
+}
